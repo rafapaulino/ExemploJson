c30062b58554f7bc9d50a89e6726471703171e51
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-10-03 09:21:49 -0300

    Initial Commit

---
+++ ExemploJsonTests/ExemploJsonTests.m
@@ -0,0 +1,32 @@
+//
+//  ExemploJsonTests.m
+//  ExemploJsonTests
+//
+//  Created by Rafael Brigagão Paulino on 03/10/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "ExemploJsonTests.h"
+
+@implementation ExemploJsonTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ExemploJsonTests");
+}
+
+@end
